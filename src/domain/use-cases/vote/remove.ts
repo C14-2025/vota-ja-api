07b943fc3c35d535fc0b5d22652e092bb4730056
Matheus <matheusandrade.ma2003@gmail.com>
2025-11-16 15:42:35 -0300

    feat: integrate event emission on create and remove vote use cases

--- src/domain/use-cases/vote/remove.ts
+++ src/domain/use-cases/vote/remove.ts
@@ -1,8 +1,14 @@
 import VoteNotFoundError from '~/domain/errors/VoteNotFoundError';
+import { IPollRepository } from '~/domain/interfaces/repositories/IPollRepository';
 import { IVoteRepository } from '~/domain/interfaces/repositories/IVoteRepository';
+import { IPollRealtimePort } from '~/domain/ports/IPollRealtimePort';
 
 export default class RemoveVoteUseCase {
-  constructor(private readonly voteRepository: IVoteRepository) {}
+  constructor(
+    private readonly voteRepository: IVoteRepository,
+    private readonly pollRepository: IPollRepository,
+    private readonly pollRealtimePort: IPollRealtimePort,
+  ) {}
 
   async execute(userId: string, pollId: string): Promise<void> {
     const existingVote = await this.voteRepository.findByUserAndPoll(
@@ -15,5 +21,21 @@ export default class RemoveVoteUseCase {
     }
 
     await this.voteRepository.delete(userId, pollId);
+
+    const pollResults = await this.pollRepository.getResultsById(pollId);
+
+    const votedOption = pollResults.options.find(
+      opt => opt.optionId === existingVote.option.id,
+    );
+
+    if (votedOption) {
+      this.pollRealtimePort.publishPollUpdate(pollId, {
+        pollId: pollId,
+        optionId: existingVote.option.id,
+        totalVotes: pollResults.totalVotes,
+        optionVotes: votedOption.voteCount,
+        percentage: votedOption.percentage,
+      });
+    }
   }
 }
