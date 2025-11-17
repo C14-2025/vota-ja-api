19e9a929d30189ad5d608b8e0a828fb07c4e3c2e
Matheus <matheusandrade.ma2003@gmail.com>
2025-11-16 21:58:57 -0300

    feat: domain list polls

--- src/domain/interfaces/repositories/IPollRepository.ts
+++ src/domain/interfaces/repositories/IPollRepository.ts
@@ -1,6 +1,8 @@
 import Poll from '~/domain/entities/Poll';
+import { IPaginationOptions, Pagination } from 'nestjs-typeorm-paginate';
 
 export interface IPollRepository {
   create(poll: Poll): Promise<Poll>;
   getById(id: string): Promise<Poll | null>;
+  findAll(options: IPaginationOptions): Promise<Pagination<Poll>>;
 }
