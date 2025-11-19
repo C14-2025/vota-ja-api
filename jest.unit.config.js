ab731e8efe10cd22e8b87bd4228f1ab1207551b8
Matheus <matheusandrade.ma2003@gmail.com>
2025-11-19 20:38:15 -0300

    feat: unit tests setup

---
+++ jest.unit.config.js
@@ -0,0 +1,33 @@
+const { testMatch } = require('./jest.e2e.config');
+
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  rootDir: '.',
+  testMatch: ['<rootDir>/tests/unit/**/*.spec.ts'],
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: [
+    'src/**/*.(t|j)s',
+    '!src/**/*.d.ts',
+    '!src/main.ts',
+    '!src/**/*.module.ts',
+    '!src/**/index.ts',
+  ],
+  coverageDirectory: './coverage',
+  coverageReporters: ['text', 'lcov', 'html'],
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  moduleNameMapper: {
+    '^~/(.*)$': '<rootDir>/src/$1',
+    '^~config/(.*)$': '<rootDir>/src/config/$1',
+    '^~domain/(.*)$': '<rootDir>/src/domain/$1',
+    '^~nestjs/(.*)$': '<rootDir>/src/nestjs/$1',
+  },
+  setupFilesAfterEnv: ['<rootDir>/tests/setup.ts'],
+  testTimeout: 10000,
+  verbose: true,
+  clearMocks: true,
+  resetMocks: true,
+  restoreMocks: true,
+};
