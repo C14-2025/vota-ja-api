044864851f98521c453434cc09aca2e0100f95ad
Matheus <matheusandrade.ma2003@gmail.com>
2025-11-30 17:19:26 -0300

    refactor: create close poll use case

---
+++ src/domain/use-cases/poll/close-poll.ts
@@ -0,0 +1,24 @@
+import { ForbiddenException, NotFoundException } from '@nestjs/common';
+import { PollStatus } from '~/domain/enums/PollStatus';
+import PollNotFoundError from '~/domain/errors/PollNotFoundError';
+import UnauthorizedPollAccessError from '~/domain/errors/UnauthorizedPollAccessError';
+import { IPollRepository } from '~/domain/interfaces/repositories/IPollRepository';
+
+export default class ClosePollUseCase {
+  constructor(private readonly pollRepository: IPollRepository) {}
+
+  async execute(pollId: string, userId: string) {
+    const poll = await this.pollRepository.getById(pollId);
+
+    if (!poll) {
+      throw new PollNotFoundError();
+    }
+
+    if (poll.creator.id !== userId) {
+      throw new UnauthorizedPollAccessError();
+    }
+
+    poll.status = PollStatus.CLOSED;
+    await this.pollRepository.save(poll);
+  }
+}
